3b9280ff0ffa9806d5f67e69a2f5567b95f61d64
Sumeet <Sumeet@ranosys.com>
2016-09-15 19:31:06 +0530

    Minor designing improvements in parcel detail.

--- Dwell/ParcelDetailViewController.m
+++ Dwell/ParcelDetailViewController.m
@@ -53,21 +53,21 @@
     //set corner radius to main background view
     _mainBackgroundView.layer.cornerRadius = cornerRadius;
     
-//    //Round status view from bottom sides
-//    _parcelStatusBackGroundView.translatesAutoresizingMaskIntoConstraints = YES;
-//    _parcelStatusBackGroundView.frame = CGRectMake(_parcelStatusBackGroundView.frame.origin.x, (_mainBackgroundView.frame.size.height+_mainBackgroundView.frame.origin.y)-49, self.view.frame.size.width-23, _parcelStatusBackGroundView.frame.size.height);
-//    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:_parcelStatusBackGroundView.bounds byRoundingCorners:( UIRectCornerBottomLeft | UIRectCornerBottomRight) cornerRadii:CGSizeMake(5.0, 5.0)];
-//        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
-//    maskLayer.frame = self.view.frame;
-//    maskLayer.path  = maskPath.CGPath;
-//    _parcelStatusBackGroundView.layer.mask = maskLayer;
+    //Round status view from bottom sides
+    _parcelStatusBackGroundView.translatesAutoresizingMaskIntoConstraints = YES;
+    _parcelStatusBackGroundView.frame = CGRectMake(_parcelStatusBackGroundView.frame.origin.x, _parcelStatusBackGroundView.frame.origin.y, self.view.frame.size.width-18, _parcelStatusBackGroundView.frame.size.height);
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:_parcelStatusBackGroundView.bounds byRoundingCorners:( UIRectCornerBottomLeft | UIRectCornerBottomRight) cornerRadii:CGSizeMake(5.0, 5.0)];
+        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    maskLayer.frame = self.view.frame;
+    maskLayer.path  = maskPath.CGPath;
+    _parcelStatusBackGroundView.layer.mask = maskLayer;
     
     //Make dots below title label
     CAShapeLayer *shapelayer = [CAShapeLayer layer];
     UIBezierPath *path = [UIBezierPath bezierPath];
     //draw a line
     [path moveToPoint:CGPointMake(0.0, _parcelTitle.frame.size.height)]; //add yourStartPoint here
-    [path addLineToPoint:CGPointMake(_parcelTitle.frame.size.width, _parcelTitle.frame.size.height)];// add yourEndPoint here
+    [path addLineToPoint:CGPointMake(self.view.frame.size.width-18, _parcelTitle.frame.size.height)];// add yourEndPoint here
     UIColor *fill = [UIColor colorWithRed:72.0/255.0 green:73.0/255.0 blue:73.0/255.0 alpha:1.0];
     shapelayer.strokeStart = 0.0;
     shapelayer.strokeColor = fill.CGColor;
